8f1268f8b73da689a95566c36cce245c6f39917b
talk2minh <talk2minh@2c1bd858-b9c5-11de-8be4-e1425cda3908>
2009-10-16 04:14:00 +0000

    import script folder

---
+++ trunk/segmentWav.pl
@@ -0,0 +1,47 @@
+use strict;
+my ($cwd, $TDTDir, $txtDir, $wavDir, $outputDir, @newsSources, $newsSource, @allASRFiles, $asr, $filename, @lines, $lineNum, $firstLine, $currentLine, $startTime, $endTime, $numLines, $inputFileName, $outputFileName, $segCount);
+$cwd = `pwd`;
+chomp($cwd);
+$TDTDir = "$cwd/../../data/TDT4_ENG_1";
+$txtDir = "$TDTDir/txt";
+$wavDir = "$TDTDir/wav";
+$outputDir = "$TDTDir/wavSeg";
+mkdir $outputDir unless -d $outputDir;
+
+@newsSources = `ls $txtDir`;
+foreach $newsSource (@newsSources){
+  chomp($newsSource);
+  mkdir "$outputDir/$newsSource" unless -d "$outputDir/$newsSource";
+  chdir "$txtDir/$newsSource";
+  @allASRFiles = `ls *.asr`;
+  foreach $asr (@allASRFiles){
+    chomp($asr);
+    print "Working on $asr\n";
+    $filename = substr $asr, 0, -4;
+    open ASR, "<$asr" or die ("Cannot open $asr");
+    @lines = <ASR>;
+    $lineNum = 1;
+    $numLines = @lines;
+    $segCount = 0;
+    while ($lineNum < $numLines){
+      #print $lineNum.'\n';
+      $firstLine = $lines[$lineNum];
+      $firstLine =~ /.+Bsec=(.+) Dur=.*/;
+      $startTime = $1;
+
+      $lineNum++;
+      while ($lineNum < $numLines && ($currentLine = $lines[$lineNum]) !~ /<X.+/){
+        $lineNum ++;
+      }
+      #now $currentLine contains silence
+      $currentLine =~ /.+Bsec=(.+) Dur=.*/;
+      $endTime = $1;
+      $inputFileName = "$wavDir/$newsSource/$filename.wav";
+      $outputFileName = "$outputDir/$newsSource/$asr"."_$segCount";
+      system("\$ESTDIR/bin/ch_wave $inputFileName -start $startTime -end $endTime -o $outputFileName");
+      print "Done with $filename\_$segCount\n";
+      $segCount ++;
+      $lineNum ++;
+    }
+  }
+}
