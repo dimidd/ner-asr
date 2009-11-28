15632ecb965ca7e1ca4b3a249ff2ff777a3270f7
talk2minh <talk2minh@2c1bd858-b9c5-11de-8be4-e1425cda3908>
2009-11-28 16:31:27 +0000

    updated convertMarkup script for stricter matching

--- scripts/convertMarkup.pl
+++ scripts/convertMarkup.pl
@@ -4,14 +4,14 @@ use strict;
 use warnings;
 
 my $xml = new XML::Simple;
-#$tagFolder = "../../data/labeled-NamedEntities/ace_tides_multling_train/DATA/ENGLISH/BNEWS";
-my $tagFolder = '.';
+my $tagFolder = "../../data/labeled-NamedEntities/ace_tides_multling_train/DATA/ENGLISH/BNEWS";
+#my $tagFolder = '.';
 my @markupFiles = `ls $tagFolder/*_APF.XML`;
 foreach my $markupFile (@markupFiles){
   chomp($markupFile);
   if ($markupFile =~ /(.*)_APF\.XML/){
-    my $srcFile = $1."\.SGM";
-    my $destFile= $1."_SIM.XML"; #SIM for simple
+    my $srcFile = "$1\.SGM";
+    my $destFile= "$1.txt"; #SIM for simple
     my @names = extractNamesFromStandoff($markupFile);
     my $namesRef = \@names;
     addTagsToSource($namesRef, $srcFile, $destFile);
@@ -32,16 +32,16 @@ sub extractNamesFromStandoff{
     #if there are more than one entity mentions in this entity
       foreach my $entityMention (@{$entityMentions}){
         if ($entityMention->{TYPE} eq 'NAM'){
-          #print $entityMention->{head}->{charseq}->{content}." occurred at ".$entityMention->{head}->{charseq}->{START}."\n";
           my $name = $entityMention->{head}->{charseq}->{content};
+          $name =~ s/\n/ /g;
           push(@names, $name);
         }
       }
     }
     else{
       if ($entityMentions->{TYPE} eq 'NAM'){
-        #print $entityMentions->{head}->{charseq}->{content}." occurred at ".$entityMentions->{head}->{charseq}->{START}."\n";
         my $name = $entityMentions->{head}->{charseq}->{content};
+        $name =~ s/\n/ /g;
         push(@names, $name);
       }
     }
@@ -62,13 +62,16 @@ sub addTagsToSource{
   chomp(@lines);
   my $doc = join(" ", @lines);
   $doc =~ s/ \<TURN\> //g;
-  print $doc."\n";
-  my $text = "";
   if ($doc =~ /\<TEXT\>(.*)\<\/TEXT\>/g){
-    $text = $text.$1;
+    my $text = $1;
     $text =~ s/^\s+//g;
+    $text =~ s/<ANNOTATION>.*?<\/ANNOTATION>//g;
     foreach my $name (@names){
-      $text =~ s/$name/\<NAME\> $name \<\/NAME\>/g;
+      #handle overlappings of tags by allowing XML tags
+      #between words in the name
+      $name =~ s/(^\s+|\s+$)//g;
+      $name =~ s/\s/\(\?:\\s\|\\s\*\\<\[\^>\]\*\?\\>\\s\*\)/g;
+      $text =~ s/([^a-zA-Z])($name)([^a-zA-Z])/$1<NAME>$2<\/NAME>$3/g;
     }
     print DEST $text;
   }
