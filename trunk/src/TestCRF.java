88a631458a107b2ca059c44e2440386df4c74124
talk2minh <talk2minh@2c1bd858-b9c5-11de-8be4-e1425cda3908>
2009-11-30 06:52:28 +0000

    added a vanilla feature to use as baseline

--- trunk/src/TestCRF.java
+++ trunk/src/TestCRF.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.*;
-
 import edu.cmu.minorthird.classify.experiments.CrossValSplitter;
 import edu.cmu.minorthird.text.*;
 import edu.cmu.minorthird.text.learn.*;
@@ -13,12 +11,14 @@ import edu.cmu.minorthird.util.gui.ViewerFrame;
 public class TestCRF {
   public static void main(String[] args){
 	  try{
+		// load data from dataDir into labels
 		File dataDir = new File("experiments/non_corrupted_data/data");
 	  	TextBaseLoader loader = new TextBaseLoader(TextBaseLoader.DOC_PER_FILE, true);
 	  	loader.load(dataDir);
 	  	BasicTextLabels labels = (BasicTextLabels)loader.getLabels();
-	  	BasicTextBase textBase = (BasicTextBase)labels.getTextBase();
-	  	/*System.out.println("Size="+textBase.size());
+	  	//BasicTextBase textBase = (BasicTextBase)labels.getTextBase();
+	  	
+	  	/*
 	  	System.out.println(labels.getTypes());
 	  	Iterator<Span> it = labels.instanceIterator("NAME");
 	  	while (it.hasNext()){
@@ -33,25 +33,29 @@ public class TestCRF {
 	  	// prepare the feature extractor
 	  	//TextLabelsAnnotatorTeacher teacher = new TextLabelsAnnotatorTeacher(
 	  		//	labels, "NAME");
-	  	SpanFE fe = new SpanFE(){
+	  	SpanFE customFE = new SpanFE(){
 	  		public void extractFeatures(TextLabels labels, Span span){
 	  			from(span).tokens().emit();
 	  			from(span).left().subSpan(-2, 2).emit();
 	  			from(span).right().subSpan(0, 2).emit();
 	  		}
 	  	};
-	  	// training a CRF
+	  	// learner that will be used in the experiment
 	  	AnnotatorLearner learner = new Recommended.CRFAnnotatorLearner();
-	  	//learner.setSpanFeatureExtractor(fe);
-	  	//learner.setAnnotationType("_prediction");
-	  	/*
-	  	Annotator ann = teacher.train(learner);
-	  	*/
+	  	// vanillaFE that's slightly different from the default FE, using a 
+	  	// window size of 4 instead of 3, and not using charTypePattern
+	  	Recommended.MultitokenSpanFE vanillaFE = new Recommended.MultitokenSpanFE();
+	  	vanillaFE.setUseCharTypePattern(false);
+	  	vanillaFE.setFeatureWindowSize(4);
+	  	// set the features that this learner will use
+	  	//learner.setSpanFeatureExtractor(customFE);
+	  	learner.setSpanFeatureExtractor(vanillaFE);
+	  	learner.setAnnotationType("_prediction");
 	  	int num_partitions = 10;
 	  	CrossValSplitter<Span> splitter=new CrossValSplitter<Span>(num_partitions);
-		//splitter.split(labels.getTextBase().documentSpanIterator());	  	
 		TextLabelsExperiment 
 		expt = new TextLabelsExperiment(labels, splitter, learner, "NAME", "_prediction");
+		
 		expt.doExperiment();
 		ExtractionEvaluation evaluation = expt.getEvaluation();	
 		new ViewerFrame("Experimental Result",new SmartVanillaViewer(evaluation));
